b5bf339b64d8bf177920106204b363bbd2c21404
lucascomamala <lucascomamala@gmail.com>
2023-04-24 16:17:07 -0600

    move_to_archive method of Item class

--- item.rb
+++ item.rb
@@ -4,7 +4,7 @@ class Item
   attr_reader :id
   attr_accessor :genre, :author, :label, :publish_date
 
-  # Contrutor for Item object
+  # Constructor for Item object
   # Dates should saved as a Date object: Date.parse(YYYY/MM/DD)
   def initialize(genre: 'Unknown', author: 'Unknown', label: 'Unknown', publish_date: 'Unknown')
     @id = Random.rand(1..1000)
@@ -15,8 +15,20 @@ class Item
     @archived = false
   end
 
+  # Archives our item if the private method returns true
+  # Doesn't return anything
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+      puts 'Item has been archived'
+    else
+      puts 'Item cannot be archived yet'
+    end
+  end
+
   private
 
+  # Returns true if publish date is greater than 10 years
   def can_be_archived?
     current_year = Date.today.year
     current_year - @publish_date.year > 10 ? true : false
