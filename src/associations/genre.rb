925f1cd43a9a6b5b47dfff460df73d732de3a722
Balikuddembe <balixemma@gmail.com>
2023-04-25 22:38:20 +0300

    implement Genre class

---
+++ src/associations/genre.rb
@@ -0,0 +1,16 @@
+require_relative '../items/item'
+class Genre
+  attr_accessor :name, :items
+  attr_reader :id, :items
+
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
+end
