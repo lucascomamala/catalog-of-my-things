0973ca4bf8fa5f89757ceea47af32f708f313b8f
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-26 02:45:29 +0500

    Create label class

--- src/items/book.rb
+++ src/items/book.rb
@@ -1,8 +1,11 @@
 require './item'
 
 class Book < Item
-  def initialize(publisher, cover_state: string)
+  attr_accessor :publisher, :name
+
+  def initialize(name, publisher, cover_state)
     super()
+    @name = name
     @publisher = publisher
     @cover_state = cover_state
   end
@@ -10,4 +13,6 @@ class Book < Item
   def can_be_archived?
     super || cover_state == 'bad'
   end
+
+  private :can_be_archived?
 end
