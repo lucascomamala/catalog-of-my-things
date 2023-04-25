2186975e657d36dc01053be04da3133d8c8c1e27
lucascomamala <lucascomamala@gmail.com>
2023-04-25 14:59:00 -0600

    Directory fix

--- src/storage.rb
+++ src/storage.rb
@@ -2,13 +2,15 @@ require 'json'
 
 class Storage
   def self.load_data(class_name)
-    return [] unless File.exist?("./data/#{class_name}.json")
+    return [] unless File.exist?(".src//data/#{class_name}.json")
 
     file = File.read("./data/#{class_name}.json")
     JSON.parse(file, create_additions: true)
   end
 
   def self.save_data(class_name, data)
-    File.write("./data/#{class_name}.json", JSON.generate(data))
+    dir = './src/data'
+    Dir.mkdir(dir) unless Dir.exist?(dir)
+    File.write("./src/data/#{class_name}.json", JSON.pretty_generate(data))
   end
 end
