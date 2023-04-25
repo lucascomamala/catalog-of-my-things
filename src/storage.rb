f931bee77544f6c0cd454f36de46be6539581eb2
lucascomamala <lucascomamala@gmail.com>
2023-04-25 14:45:53 -0600

    Add self methods

--- src/storage.rb
+++ src/storage.rb
@@ -1,14 +1,14 @@
 require 'json'
 
 class Storage
-  def load_data(class_name)
+  def self.load_data(class_name)
     return [] unless File.exist?("./data/#{class_name}.json")
 
     file = File.read("./data/#{class_name}.json")
     JSON.parse(file, create_additions: true)
   end
 
-  def save_data(class_name, data)
+  def self.save_data(class_name, data)
     File.write("./data/#{class_name}.json", JSON.generate(data))
   end
 end
