c3d36d1a0a9294948d188cc9b1454fc8ec806afd
lucascomamala <lucascomamala@gmail.com>
2023-04-26 12:50:13 -0600

    Linters

--- src/spec/game_spec.rb
+++ src/spec/game_spec.rb
@@ -2,7 +2,7 @@ require_relative '../items/game'
 
 RSpec.describe Game do
   before(:all) do
-    @game = Game.new(publish_date: Date.parse("2010/1/1"), multiplayer: false, last_played: Date.parse("2020/1/1"))
+    @game = Game.new(publish_date: Date.parse('2010/1/1'), multiplayer: false, last_played: Date.parse('2020/1/1'))
   end
 
   describe '#new' do
@@ -16,15 +16,15 @@ RSpec.describe Game do
       expect(@game.send(:can_be_archived?)).to be_truthy
     end
 
-    it "should return false if last_played_at is younger than 2 years" do
+    it 'should return false if last_played_at is younger than 2 years' do
       @game.last_played = Date.today
       expect(@game.send(:can_be_archived?)).to be_falsey
     end
 
-    it "should return false if publish_date is younger than 10 years" do
-      @game.last_played = Date.parse("2020/1/1")
-      @game.publish_date = Date.parse("2019/1/1")
+    it 'should return false if publish_date is younger than 10 years' do
+      @game.last_played = Date.parse('2020/1/1')
+      @game.publish_date = Date.parse('2019/1/1')
       expect(@game.send(:can_be_archived?)).to be_falsey
-    end 
+    end
   end
 end
