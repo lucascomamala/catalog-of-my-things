5efb3e18bfb4f5413c1e58422a2373faf3b959a7
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-26 23:12:33 +0500

    Add methods to load books and labels

--- src/modules/book_module.rb
+++ src/modules/book_module.rb
@@ -8,10 +8,13 @@ module BookModule
     print 'Publisher: '
     publisher = gets.chomp
 
-    print 'Cover State(good/bad): '
+    print 'Cover State (good/bad): '
     cover_state = gets.chomp.downcase
 
-    book = Book.new(title, publisher, cover_state)
+    print 'Publish Date (YYYY/MM/DD): '
+    publish_date = gets.chomp.to_s
+
+    book = Book.new(title, publisher, cover_state, publish_date)
     @books << book
 
     puts 'Book added successfully'
@@ -20,7 +23,7 @@ module BookModule
   def list_books
     puts 'No Books added yet! Please add some books' if @books.empty?
     @books.each do |book|
-      puts "Title: #{book.title}, Publisher: #{book.publisher}, Cover State: #{book.cover_state.upcase}"
+      puts "Title: #{book.title}, Publisher: #{book.publisher}, Cover State: #{book.cover_state.upcase}, Publish Date: #{book.publish_date}"
     end
   end
 end
--- src/storage/storage.rb
+++ src/storage/storage.rb
@@ -1,28 +1,51 @@
 require 'json'
+require 'fileutils'
+
 module Storage
-    def preserve_data
-        puts "All the data has been saved successfully.\n"
-        save_books
-        save_labels
-    end
+  def preserve_data
+    puts "All the data has been saved successfully.\n"
+    save_books
+    save_labels
+  end
 
-    def save_books
-        file = './src/json/books.json'
-        books_object = []
-        @books.each { |book| books_object << { title: book.title, publisher: book.publisher, cover_state: book.cover_state, publish_date: book.publish_date} }
-        file.write{file, books_object.to_json}
+  def save_books
+    file_path = './src/json/books.json'
+    books_object = []
+    @books.each do |book|
+      books_object << { title: book.title, publisher: book.publisher, cover_state: book.cover_state,
+                        publish_date: book.publish_date }
     end
+    FileUtils.mkdir_p(File.dirname(file_path))
+    File.write(file_path, books_object.to_json)
+  end
 
-    def save_labels
-        file = './src/json/labels.json'
-        labels_object = []
-        @labels.each { |label| labels_object << { title: label.title, color: label.color } }
-        file.write{file, labels_object.to_json}
-    end
+  def save_labels
+    file_path = './src/json/labels.json'
+    labels_object = []
+    @labels.each { |label| labels_object << { title: label.title, color: label.color } }
+    FileUtils.mkdir_p(File.dirname(file_path))
+    File.write(file_path, labels_object.to_json)
+  end
 
-    def load_books
+  def load_books
+    file = './src/json/books.json'
+    list_books = []
+    if File.exist?(file) && !File.empty?(file)
+      data = JSON.parse(File.read(file))
+      data.each do |book|
+        list_books << Book.new(book['title'], book['publisher'], book['cover_state'], book['publish_date'])
+      end
     end
+    list_books
+  end
 
-    def load_labels
+  def load_labels
+    file = './src/json/labels.json'
+    list_labels = []
+    if File.exist?(file) && !File.empty?(file)
+      data = JSON.parse(File.read(file))
+      data.each { |label| list_labels << Label.new(label['title'], label['color']) }
     end
+    list_labels
+  end
 end
