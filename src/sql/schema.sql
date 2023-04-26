4fb90a1cdebee3e57764909291fb647537ddccea
lucascomamala <lucascomamala@gmail.com>
2023-04-26 12:58:28 -0600

    Add table for game

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -8,3 +8,16 @@ CREATE TABLE item (
   archived BOOLEAN,
   PRIMARY KEY(id)
 );
+
+CREATE TABLE game (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100),
+  genre INT REFERENCES genre(id),
+  author INT REFERENCES author(id),
+  label INT REFERENCES label(id),
+  publish_date DATE,
+  archived BOOLEAN,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  PRIMARY KEY(id)
+);
