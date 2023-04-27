ce4ab987e9edab23dbffa51f3996daec2a48edca
Balikuddembe <balixemma@gmail.com>
2023-04-27 03:10:35 +0300

    add music album and genre class tables

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -50,3 +50,19 @@ CREATE TABLE books (
   cover_state VARCHAR(4) NOT NULL,
   PRIMARY KEY(id)
 );
+
+-- Music Album class table
+CREATE TABLE music_album (
+  id SERIAL PRIMARY KEY,
+  item_id int FOREIGN KEY REFERENCES item(id),
+  on_spotify BOOLEAN,
+  publish_date DATE,
+  archived BOOLEAN,
+);
+
+-- Genre class table
+CREATE TABLE genre (
+  id SERIAL PRIMARY KEY,
+  item_id int FOREIGN KEY REFERENCES item(id),
+  name VARCHAR(100)
+);
